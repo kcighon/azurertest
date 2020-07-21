22b7d97d8c41375ecf5c7ef93a6a4b27904015cd
J <kcighon@gmail.com>
2020-07-21 13:42:37 +0100

    Update installiis.ps1

--- installiis.ps1
+++ installiis.ps1
@@ -1,2 +1,6 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
+$op = "" | select Hostname, IP
+$op.Hostname = gwmi Win32_ComputerSystem | select -exp Name
+$op.IP = (gwmi Win32_NetworkAdapterConfiguration | ?{$_.IPEnabled} | select -ExpandProperty IpAddress) -join ","
+$op | ConvertTo-Html | Out-File "C:\inetpub\wwwroot\Default.htm"
